4cc0f92e1d505528dc1b29e09b6ad86d3d007341
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2025-10-06 15:47:55 -0400

    MVP - foorter add social_media links

--- theme/template-parts/layout/footer-content.php
+++ theme/template-parts/layout/footer-content.php
@@ -36,11 +36,41 @@
 
 		<div class="stack">
 			<h2 class="text-step-0 font-content">Operating Hours</h2>
-			<?php if ($hours = get_theme_mod('business_hours')) : ?>
-				<p><?php echo nl2br(esc_html($hours)); ?></p>
-			<?php endif; ?>
+			<div>
+				<?php if ($hours = get_theme_mod('business_hours')) : ?>
+					<p><?php echo nl2br(esc_html($hours)); ?></p>
+				<?php endif; ?>
+				<p>After Hours by Appointment</p>
+			</div>
+			<div>
+				<p>Follow us for results and tips</p>
+				<?php
+				$args = array(
+					'post_type'  => 'social_media_link',
+					'posts_per_page' => 5,
+					// Several more arguments could go here. Last one without a comma.
+				);
 
-			<p>After Hours by Appointment</p>
+				// Query the posts:
+				$social_media_query = new WP_Query($args);
+
+				// Loop through the Service:
+				while ($social_media_query->have_posts()) :
+					$social_media_query->the_post();
+					// Echo some markup
+					$social_link = get_post_meta($post->ID, 'social_media_link')[0];
+					$social_icon = get_post_meta($post->ID, 'social_media_icon')[0];
+
+				?>
+					<a href=<?php echo esc_url($social_link) ?>><i class="fa fa-<?php echo esc_textarea($social_icon); ?>"></i></a>
+
+				<?php
+				endwhile;
+
+				// Reset Post Data
+				wp_reset_postdata();
+				?>
+			</div>
 		</div>
 		<div class="stack">
 			<h2 class="text-step-0 font-content">Contact</h2>
