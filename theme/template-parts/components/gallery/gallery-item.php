d973ba27c859214ce375506bc82dafe5793f054c
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2025-10-12 07:01:53 -0400

    MVP - Gallery(2)

--- theme/template-parts/components/gallery/gallery-item.php
+++ theme/template-parts/components/gallery/gallery-item.php
@@ -1,4 +1,4 @@
-<div class="stack">
-    <p><?php echo $args['title'] ?></p>
+<div class="stack text-center" style="--stack-spacing: var(--spacing-s);">
     <?php echo wp_get_attachment_image($args['image'], 'full'); ?>
+    <p><?php echo $args['title'] ?></p>
 </div>
