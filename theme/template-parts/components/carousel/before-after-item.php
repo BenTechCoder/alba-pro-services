3cb59bcbc68bae729519c6a7b8296b0f1f8a36f0
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2025-10-19 16:20:14 -0400

    FIX: Fix figcaptions and add underline Carousel Items

--- theme/template-parts/components/carousel/before-after-item.php
+++ theme/template-parts/components/carousel/before-after-item.php
@@ -4,15 +4,15 @@ if ($args['type'] === 'before_after') {
    <div class="carousel-item-container">
         <p class="carousel-service center text-center"><?php echo get_the_title($args['service']); ?></p>
         <div class="carousel-item grid text-center" aria-hidden="true" data-layout="50-50" style="--grid-min-item-size: 25rem;">    
-        <figure>
+        <figure class="stack" style="--stack-spacing: var(--spacing-s)">
                 <figcaption>
-                    <a>Before</a>
+                    <p class="underline decoration-primary">Before</p>
                 </figcaption>
                 <?php echo wp_get_attachment_image($args['before_image'], 'full'); ?>
             </figure>
-            <figure>
+            <figure class="stack" style="--stack-spacing: var(--spacing-s)">
                 <figcaption>
-                    <a>After</a>
+                    <p class="underline decoration-primary">After</p>
                 </figcaption>
                 <?php echo wp_get_attachment_image($args['after_image'], 'full'); ?>
             </figure>
